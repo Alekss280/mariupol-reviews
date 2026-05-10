62b892f6fb988debe1cbfdd64e8c9650e0d8c887
Alekss280 <tsekhmeistruk28@mail.ru>
2026-05-10 18:10:02 +0300

    Добавлены функции для отображения списка отзывов и добавления анонимных отзывов

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
 
-# Create your views here.
+def index(request):
+    """Главная страница со списком отзывов (последние сверху)"""
+    reviews = Review.objects.all().order_by('-created_at')
+    return render(request, 'reviews/index.html', {'reviews': reviews})
+
+def add_review(request):
+    """Страница добавления анонимного отзыва"""
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')   # после сохранения перенаправляем на главную
+    else:
+        form = ReviewForm()
+    return render(request, 'reviews/add_review.html', {'form': form})
