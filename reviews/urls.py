76138d0b481fbff92f56e2a361859112d2aebd69
Alekss280 <tsekhmeistruk28@mail.ru>
2026-05-11 15:54:33 +0300

    Добавлен маршрут для страницы статистики отзывов

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_review, name='add_review'),
+    path('stats/', views.stats, name='stats'),
 ]
