4d5bed631c53ee9f8ba9c53fdbc59f21d420ce4a
Alekss280 <tsekhmeistruk28@mail.ru>
2026-05-11 18:18:20 +0300

    Добавлен маршрут для отображения деталей предприятия по его идентификатору

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_review, name='add_review'),
     path('stats/', views.stats, name='stats'),
+    path('enterprise/<int:enterprise_id>/', views.enterprise_detail, name='enterprise_detail'),
 ]
