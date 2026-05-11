a60a6f8cbd5b656ccb80cb9b9a70db1db5268fa7
Alekss280 <tsekhmeistruk28@mail.ru>
2026-05-11 22:12:30 +0300

    Обновлены настройки безопасности: отключен режим отладки и добавлены разрешенные хосты.

--- mariupol_portal/settings.py
+++ mariupol_portal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l0^7uakub9u*+x_uetsmx)h-#$(-c&0d_(8f7l4$#-3-l5l%2t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mariupol-reviews.pythonanywhere.com']
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
