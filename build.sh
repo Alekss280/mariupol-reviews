d3d2dac076ee90dc508433de6a34d80c0475b85e
Alekss280 <tsekhmeistruk28@mail.ru>
2026-05-24 00:22:57 +0300

    Фикс время, улучшены команды сборки.

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-pip install --upgrade -r requirements.txt && \
+# Основной скрипт для сборки образа Docker. Устанавливает зависимости, загружает модель и выполняет миграции.
+
+# Устанавливаем последнюю версию pip
+
+pip install --upgrade pip && \
+
+# Устанавливаем зависимости из файла requirements.txt
+
+pip install -r requirements.txt --noinput && \
+
+# Выполняем миграции и собираем статические файлы для Django
+
 python manage.py migrate --noinput && \
+
+# Собираем статические файлы для Django
+
 python manage.py collectstatic --noinput
